b138e79e2901dd35e39e4942dfbb95bbe6903c36
Bryce Dumas <bryce.dumas@gmail.com>
2020-04-24 14:39:08 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -20,7 +20,7 @@ class CashRegister
       hundred_float = 100.0
       pay_percent = (hundred_float - discount.to_f)/hundred_float
       @total *= pay_percent
-      puts "After the discount, the total comes to $#{@total}."
+      "After the discount, the total comes to $#{@total}."
     end
   end
 end
